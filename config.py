27064638dfe38d185461d493c45f8e946d77d022
Temola <dinahiliya91@gmail.com>
2025-11-12 06:29:37 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'afabros_computer_college_secret_key_2024'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///cbt_exam.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
